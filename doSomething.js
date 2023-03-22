e900e8927ae37f7eba5191571980d196f3b12009
svdegron <svdegron@gmail.com>
2023-03-23 01:23:31 +0500

    Some thing

---
+++ doSomething.js
@@ -0,0 +1,13 @@
+funcion doSomething (firstVar) {
+	var secondVar = 0;
+	for (var i = 0; i < 10; i++) {
+		secondVar = secondVar * i;
+		console.log(i, secondVar);
+	}
+}
+// commentary
+var newVar = 5;
+new line 1
+new line 5
+add new line
+add new 2 line
